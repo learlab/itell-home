0fb5e4406e80ae8e3e4cf348f2894f9034426ed9
yanqiushi-mw <qiushi.yann@gmail.com>
2025-11-07 16:45:09 -0600

    remove pdf upload from home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { Hero } from '@/components/Hero'
 import LearnerFeatures from '@/components/learner-features'
 import PrimaryFeatures from '@/components/primary-features'
 import { Testimonials } from '@/components/testimonials'
-import { PdfUpload } from '@/components/upload/pdf-upload'
 
 export default function Home() {
   return (
@@ -15,7 +14,7 @@ export default function Home() {
         <Header />
         <div className="[&>section:nth-child(even)]:bg-slate-50 [&>section:nth-child(odd)]:bg-white">
           <Hero />
-          <PdfUpload />
+          {/*<PdfUpload />*/}
           <PrimaryFeatures />
           <LearnerFeatures />
           <Testimonials />
