cbf9824d7d3b504aa4f8308d92094ff60063a704
Wesley Morris <tiedaar@gmail.com>
2025-11-12 14:53:25 -0600

    change title

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -1,10 +1,10 @@
 export const blogPosts = [
   {
     id: 1,
-    title: "Why We're Reimagining Training to Save Lives",
+    title: "Learning Saves Lives: Why we are reimagining workforce training",
     excerpt: 'Inspired by a preventable tragedy, iTELL uses AI to make vital training impossible to ignore, turning static manuals into engaging experiences that protect people and prevent accidents.',
     content: `
-      <b>Why We're Reimagining Training to Save Lives</b><br>
+      <b>Learning Saves Lives: Why we are reimagining workforce training</b><br>
       <i>By Wesley Morris, Entrepreneurial Lead of iTELL</i><br><br>
       Fifteen years ago, a conversation around a bonfire changed my life.<br><br>
       My cousin, a young Army lieutenant on leave from Iraq, was wrestling with a tragedy. 
