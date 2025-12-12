b3ef28e195cd7cce4b7bf5f4aeb3513dcda60c25
Wesley Morris <tiedaar@gmail.com>
2025-12-12 13:18:14 -0600

    remove cookie banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { type Metadata } from 'next'
 import { Roboto_Slab, Roboto } from 'next/font/google'
 import clsx from 'clsx'
 import { GoogleAnalytics } from '@next/third-parties/google'
-import TermlyCMP from '@/components/TermlyCMP'
+// import TermlyCMP from '@/components/TermlyCMP'
 import '@/styles/tailwind.css'
 import { Toaster } from 'sonner'
 import { Header } from '@/components/Header'
@@ -10,7 +10,7 @@ import { Footer } from '@/components/Footer' // Import your Footer if you have o
 import { QueryProvider } from '@/components/providers/query-provider'
 import { Suspense } from 'react'
 
-const WEBSITE_UUID = 'c97ff573-1139-4e61-aa4c-11639c6818c5'
+// const WEBSITE_UUID = 'c97ff573-1139-4e61-aa4c-11639c6818c5'
 
 export const metadata: Metadata = {
   title: {
@@ -51,13 +51,13 @@ export default function RootLayout({
     >
       <body className="flex h-full flex-col">
         {/* Cookie Consent Banner */}
-        <Suspense fallback={null}>
+        {/* <Suspense fallback={null}>
           <TermlyCMP 
             websiteUUID={WEBSITE_UUID}
             autoBlock={true}
             masterConsentsOrigin="https://app.termly.io"
           />
-        </Suspense>
+        </Suspense> */}
         
         <QueryProvider>
           <Header />
